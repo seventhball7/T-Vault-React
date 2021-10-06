9bcc22fae9effe970cdc3dc10b4e377bb2615e1b
DESKTOP-6EQ384F\Piktorlabs <shahankhan485@gmail.com>
2021-10-06 21:56:54 +0530

    Fixed Appjs file structure

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="wrapper">
       <NavBar />
-      <div className="safeitems">
-        <Safes />
-      </div>
+
+      <Safes />
+
       <Secret />
     </div>
   );
